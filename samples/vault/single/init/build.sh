3600aebab3734a55832f066e9e14441a78588ada
Gusztáv Varga <mail@gusztavvargadr.me>
2022-08-20 22:50:32 +0200

    Update Vault samples to use Docker

--- samples/vault/single/init/build.sh
+++ samples/vault/single/init/build.sh
@@ -15,11 +15,11 @@ export VAULT_ZIP_FILE=vault_${VAULT_VERSION}_linux_amd64.zip
 export VAULT_BINARY_FILE=vault
 export VAULT_DOWNLOAD_URI=https://releases.hashicorp.com/vault/$VAULT_VERSION/$VAULT_ZIP_FILE
 
-if [ ! -f /usr/local/bin/$VAULT_BINARY_FILE ]; then
+if [ ! -f /usr/bin/$VAULT_BINARY_FILE ]; then
   mkdir -p /tmp/vault/
   cd /tmp/vault/
   wget -q $VAULT_DOWNLOAD_URI
   unzip $VAULT_ZIP_FILE
   rm $VAULT_ZIP_FILE
-  mv ./$VAULT_BINARY_FILE /usr/local/bin/
+  mv ./$VAULT_BINARY_FILE /usr/bin/
 fi

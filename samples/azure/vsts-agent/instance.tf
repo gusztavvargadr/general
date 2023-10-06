63f47543267e0632bc3826a2b85d704ee3ee8499
Gusztáv Varga <mail@gusztavvargadr.me>
2023-10-06 20:23:05 +0200

    Add spot support for CherryServers

--- samples/azure/vsts-agent/instance.tf
+++ samples/azure/vsts-agent/instance.tf
@@ -8,12 +8,24 @@ variable "instance_image" {
   default = "ubuntu_20_04"
 }
 
+variable "instance_spot" {
+  type    = bool
+  default = false
+}
+
+variable "instance_userdata" {
+  type    = string
+  default = ""
+}
+
 locals {
   instance_region = local.region_name
   instance_name   = local.deployment_name
 
   instance_plan     = var.instance_plan
   instance_image    = var.instance_image
+  instance_spot     = var.instance_spot
+  instance_userdata = var.instance_userdata
   instance_ssh_keys = local.ssh_key_id
 }
 
@@ -24,18 +36,21 @@ resource "terraform_data" "instance" {
 
     local.instance_plan,
     local.instance_image,
+    local.instance_spot,
+    local.instance_userdata,
+    local.instance_ssh_keys,
   ]
 
   provisioner "local-exec" {
     command = format(
-      "cherryctl server create --region %v --hostname %v --plan %v --image %v --ssh-keys %v %v %v --output json | jq -r .id | tee ${path.root}/.terraform/instance-id-${self.id}",
+      "cherryctl server create --region %v --hostname %v --plan %v --image %v %v %v --ssh-keys %v --output json | jq -r .id | tee ${path.root}/.terraform/instance-id-${self.id}",
       local.instance_region,
       local.instance_name,
       local.instance_plan,
       local.instance_image,
+      local.instance_spot ? "--spot-instance" : "",
+      local.instance_userdata != "" ? "--userdata-file ${local.instance_userdata} " : "",
       local.instance_ssh_keys,
-      "", # "--spot-instance",
-      "", # --userdata-file ~/src/gusztavvargadr/chef/samples/policies/vsts-agent/cloud-init.yml
     )
   }
 

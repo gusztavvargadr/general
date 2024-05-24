2b9678acd176178129fc1e3ef4334207b3e5b21a
Gusztáv Varga <mail@gusztavvargadr.me>
2024-05-25 00:06:05 +0200

    Consolidate LetsEncrypt sample

---
+++ samples/letsencrypt/core/terraform.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~> 1.8"
+
+  required_providers {
+    acme = {
+      source  = "vancluever/acme"
+      version = "~> 2.21"
+    }
+
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.2"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+
+  backend "consul" {
+    path = "gusztavvargadr-general.letsencrypt-core/terraform/state"
+    gzip = true
+  }
+}
+
+provider "acme" {
+  server_url = local.acme.server
+}
